73aceee2abb18a85d24839ed617a8df12afebda1
spscah <cah@stpaulsschool.org.uk>
2022-05-09 10:52:43 +0100

    Add files via upload

---
+++ IHashTable.cs
@@ -0,0 +1,17 @@
+﻿namespace HashTable.Lib
+{
+    public interface IHashTable<T> 
+        where T : IEquatable<T>
+    {
+        T Add(T value);
+        bool IsPresent(T value);
+        void Rebalance();
+    }
+
+    public interface SPSStudent
+    {
+        string Name { get; }
+        string Year { get; }
+        string Tutor { get; }
+    }
+}
